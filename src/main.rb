3508b88e5ef83a4babb4002c2242d972f50de4d0
Kersul <joaohkfaria@gmail.com>
2016-07-22 22:19:44 -0300

    Removendo .idea, melhorando automato

--- src/main.rb
+++ src/main.rb
@@ -2,6 +2,11 @@ require './automata'
 
 class Main
 
+  # Setando a codificação padrão do Ruby
+  Encoding.default_internal = Encoding::UTF_8
+  Encoding.default_external = Encoding::UTF_8
+
+  # Começando programa
   puts "",
        "Projeto PCS2427 - Simulação de Autômatos",
        "João Henrique Kersul Faria - 8041157",
@@ -9,12 +14,12 @@ class Main
 
   puts "Digite o nome do arquivo .txt que deseja carregar (não precisa colocar a extensão):"
 
+  # Pegando nome do arquivo do usuário
   file_name = "../" + gets.chomp + ".txt"
 
+  # Se o arquivo existir, começamos a construir o automato
   if File.exist? file_name
     Automata.construct(file_name)
-
-    puts Automata.automata_data.to_s
   else
     puts "Arquivo não existe, finalizando programa."
   end
